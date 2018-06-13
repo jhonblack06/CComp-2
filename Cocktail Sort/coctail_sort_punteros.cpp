7190c356af433984072888bd21353c7be51bc8a7
Brandon Valencia <31133090+jhonblack06@users.noreply.github.com>
2018-06-12 19:33:05 -0500

    Update coctail_sort_punteros.cpp

--- Cocktail Sort/coctail_sort_punteros.cpp
+++ Cocktail Sort/coctail_sort_punteros.cpp
@@ -1,16 +1,31 @@
-
 #include <iostream>
 using namespace std;
 
-void CocktailSort(int a[], int n)
+bool comp(int a, int b,bool m){
+    if(m){
+        if(a > b){return true;}
+        else{return false;}
+    }
+    else{
+        if(a < b){return true;}
+        else{return false;}
+    }
+}
+
+
+
+
+void CocktailSort(int a[], int n,bool m)
 {
     bool swapped = true;
-    int *start = a; 
+    bool (*fun)(int,int,bool);
+    fun=comp;
+    int *start = a;
     int *end = start +(n - 1);
     while (swapped) {
         swapped = false;
         for (int *i=start; i < end; ++i) {
-            if (*i > *(i + 1)) {
+            if (fun(*i,*(i + 1),m)) {
                 swap(*i, *(i + 1));
                 swapped = true;
             }
@@ -21,7 +36,7 @@ void CocktailSort(int a[], int n)
         swapped = false;
         end-=1;
         for (int *i = (end - 1); i >= start;i--) {
-            if (*i > *(i + 1)) {
+            if (fun(*i,*(i + 1),m)) {
                 swap(*i, *(i + 1));
                 swapped = true;
             }
@@ -42,8 +57,10 @@ int main()
 {
     int a[] = { 5, 1, 4, 2, 8, 0, 2 };
     int n = sizeof(a) / sizeof(a[0]);
-    CocktailSort(a , n);
+    bool asc = 0;
+    CocktailSort(a , n ,asc);
     cout << "Lista ordenada: " << endl;
     imprimir(a , n);
     return 0;
 }
+
